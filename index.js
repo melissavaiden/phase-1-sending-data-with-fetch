9b15c7b12aa6fcf0d6d6efd6f5d256fe456c250c
Melissa Vaiden <melissavaiden@gmail.com>
2022-02-26 16:26:11 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    const configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            "name": userName,
+            "email": userEmail,
+        })
+    }
+    return fetch('http://localhost:3000/users', configObj)
+    .then (resp => resp.json())
+    .then (body => newContent(body.id))
+    .catch(response =>  document.body.append('Unauthorized Access'))
+}
+
+    function newContent(newID) {
+    document.getElementsByTagName('body')[0].innerHTML = newID;
+}
